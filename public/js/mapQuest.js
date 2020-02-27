9191d0e5342f5717c0d37f3e36e624415de13180
Beverly Li <bel058@ucsd.edu>
2020-02-26 20:17:36 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = '4Z14mnizfyotxl9AiM6CsOW5bNgyUwYQ';
+
+   var map = L.mapquest.map('map', {
+      center: [32.88, -117.236],
+      layers: L.mapquest.tileLayer('map'),
+      zoom: 14,
+      zoomControl: false
+   });
+
+   L.marker([32.88, -117.236]).addTo(map);
+
 }
